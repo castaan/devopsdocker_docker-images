9bd0894c606c12ebf027aabdf0d79969d1c2ccda
Jorge Mascarell <jorge.mascarell@ext.euipo.europa.eu>
2018-02-15 15:03:28 +0100

    Change test URL to OHIM Portal, accessible from test en prod environments

--- cypress/test/cypress/integration/bamboo/00_startup_screen.js
+++ cypress/test/cypress/integration/bamboo/00_startup_screen.js
@@ -1,14 +1,14 @@
 //
-// **** Bamboo Startup Screen Tests ****
+// **** OHIM Portal Startup Screen Tests ****
 //
 
-describe('Bamboo Startup Screen', function(){
+describe('OHIM Portal Startup Screen', function(){
 
   it('Loading Startup Screen', function(){
     cy.fixture('env').then((json) => {
-        cy.visit('/bamboo/userlogin!default.action')
+        cy.visit('/ohimportal/es/home')
         cy.wait(json.short_wait)
-        cy.screenshot('00 - Bamboo Startup Screen')
+        cy.screenshot('00 - OHIM Portal Startup Screen')
     })
   })
 
