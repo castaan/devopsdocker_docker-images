d4664c3c0cb48ba57e0f0fbedc55e1e1f5f14e24
Jorge Mascarell <jorge.mascarell@ext.euipo.europa.eu>
2018-02-15 11:03:32 +0100

    Fixing connection to bamboo server

--- cypress/test/cypress/integration/bamboo/00_startup_screen.js
+++ cypress/test/cypress/integration/bamboo/00_startup_screen.js
@@ -6,8 +6,7 @@ describe('Bamboo Startup Screen', function(){
 
   it('Loading Startup Screen', function(){
     cy.fixture('env').then((json) => {
-        Cypress.config('baseUrl', json.bamboo.url)
-        cy.visit('/userlogin!default.action')
+        cy.visit('/bamboo/userlogin!default.action')
         cy.wait(json.short_wait)
         cy.screenshot('00 - Bamboo Startup Screen')
     })
